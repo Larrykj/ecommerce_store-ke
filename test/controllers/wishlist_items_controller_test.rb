cfc4a9e859a3f6f2c10aa45e72b1187a8d281897
Larrykj <larrykings2002@gmail.com>
2026-02-02 09:08:51 +0300

    Fixed CI/CD pipelines for test

--- test/controllers/wishlist_items_controller_test.rb
+++ test/controllers/wishlist_items_controller_test.rb
@@ -1,18 +1,46 @@
 require "test_helper"
 
 class WishlistItemsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    @product = products(:one)
+    sign_in @user
+  end
+
   test "should get index" do
-    get wishlist_items_index_url
+    get wishlist_items_url
     assert_response :success
   end
 
-  test "should get create" do
-    get wishlist_items_create_url
-    assert_response :success
+  test "should create wishlist_item" do
+    assert_difference("WishlistItem.count") do
+      post wishlist_items_url, params: { product_id: products(:two).id }
+    end
+
+    assert_redirected_to products_path
   end
 
-  test "should get destroy" do
-    get wishlist_items_destroy_url
-    assert_response :success
+  test "should destroy wishlist_item" do
+    wishlist_item = WishlistItem.create!(user: @user, product: @product)
+    
+    assert_difference("WishlistItem.count", -1) do
+      delete wishlist_item_url(wishlist_item)
+    end
+
+    assert_redirected_to products_path
+  end
+
+  test "should require authentication for index" do
+    sign_out @user
+    get wishlist_items_url
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should require authentication for create" do
+    sign_out @user
+    post wishlist_items_url, params: { product_id: @product.id }
+    assert_redirected_to new_user_session_path
   end
 end
